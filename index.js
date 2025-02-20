82d805a9415e4e1646c50af54f531aed9abf6f14
Daniel Radezky <danielradezky@gmail.com>
2025-02-20 16:01:39 -0500

    Get rid of the endpoints that can be served by express.static

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = 3000
 
 app.use(cookieParser())
-app.use(express.static('public')) // Serve static HTML & PDFs from 'public' folder
+app.use(express.static('static/public')) // Serve static HTML & PDFs from 'public' folder
 
 // Middleware for simple auth
 app.use((req, res, next) => {
@@ -18,23 +18,14 @@ app.use((req, res, next) => {
 
 
 app.get('/', (req, res) => {
-  res.set('X-Custom-Header', 'CrawlerTest');
-  // res.status(418).send('I am a teapot')
-  const filePath = path.join(__dirname, 'public/page.html');
+  const filePath = path.join(__dirname, 'static/page.html');
   res.sendFile(filePath);
 })
-
-
-app.get('/simple.pdf', (req, res) => {
-  const filePath = path.join(__dirname, 'public/simple.pdf');
-  res.sendFile(filePath);
-})
-
 
 // Auth simulation (login sets a cookie)
 app.post('/login', (req, res) => {
   res.cookie('auth', 'true', { httpOnly: true });
-  res.send('Logged in');
+  res.redirect('/auth/landing')
 })
 
 // Logout
@@ -42,22 +33,5 @@ app.post('/logout', (req, res) => {
   res.clearCookie('auth')
   res.send('Logged out')
 })
-
-app.get('/auth/page1', (req, res) => {
-  res.set('X-Custom-Header', 'CrawlerTest');
-  const filePath = path.join(__dirname, 'public/page1.html');
-  res.sendFile(filePath);
-})
-
-app.get('/auth/protected.pdf', (req, res) => {
-  const filePath = path.join(__dirname, 'public/protected.pdf');
-  res.sendFile(filePath);
-})
-
-// Custom response headers & status control
-// app.get('/custom-response', (req, res) => {
-//   res.set('X-Custom-Header', 'CrawlerTest')
-//   res.status(418).send('I am a teapot')
-// })
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
